75dec7aee11d60772cf404f935f442bfb69efdf5
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-11-25 23:02:34 +0530

    middelware / protected route aded

---
+++ apps/main-web/app/middleware.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(req:NextRequest){
+    const token=req.cookies.get("token")?.value;
+
+    const publicRoutes= ["/auth/signin","/auth/signup"];
+
+    if(publicRoutes.includes(req.nextUrl.pathname)){
+        if(token){
+            return NextResponse.redirect(new URL("/",req.url));
+
+        }
+
+        return  NextResponse.next();
+    }
+
+    if(!token){
+        return NextResponse.redirect(new URL("/auth/signin", req.url));
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/", "/call/:path*"],
+};
